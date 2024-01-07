89a1d8ef10848c6ca0a2af5f12ab2e22515c45d7
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-01-07 22:29:47 +0100

    Update enkripcija.js

--- enkripcija.php
+++ enkripcija.php
@@ -1,40 +1,37 @@
-<?php
-$linija = $pom = $kr = '';
-$duz = $i = $k = $k1 = $k2 = 0;
+const fs = require('fs');
+const readlineSync = require('readline-sync');
 
-$dat = fopen("ot.txt", "r");
-if ($dat) {
-    while (($linija = fgets($dat)) !== false) {
-        echo $linija . PHP_EOL;
-        $pom .= $linija . PHP_EOL;
-    }
-    fclose($dat);
-} else {
-    echo "Datoteka ne postoji.";
+let linija = pom = kr = '';
+let duz = i = k = k1 = k2 = 0;
+
+try {
+    const dat = fs.readFileSync('ot.txt', 'utf8');
+    linija = dat;
+    pom = dat;
+    console.log(linija);
+
+} catch (err) {
+    console.error("Datoteka ne postoji.");
 }
 
-echo "Unesite kljuc: ";
-$k = intval(trim(fgets(STDIN)));
+k = parseInt(readlineSync.question("Unesite kljuc: "));
 
-for ($i = 0; $i < strlen($pom); $i++) {
-    if (ord($pom[$i]) > 96 && ord($pom[$i]) < 123) {
-        $k1 = ord($pom[$i]);
-        $k1 = $k1 + $k;
-        if ($k1 > 122) {
-            $pom[$i] = chr($k1 - 122 + 97);
+for (i = 0; i < pom.length; i++) {
+    if (pom.charCodeAt(i) > 96 && pom.charCodeAt(i) < 123) {
+        k1 = pom.charCodeAt(i);
+        k1 = k1 + k;
+        if (k1 > 122) {
+            pom = pom.substring(0, i) + String.fromCharCode(k1 - 122 + 97) + pom.substring(i + 1);
         } else {
-            $pom[$i] = chr($k1);
+            pom = pom.substring(0, i) + String.fromCharCode(k1) + pom.substring(i + 1);
         }
-        $kr .= $pom[$i];
+        kr += pom[i];
     }
 }
 
-$dat2 = fopen("kr.txt", "w");
-if ($dat2) {
-    fwrite($dat2, $kr . PHP_EOL);
-    fclose($dat2);
-    echo "Datoteka kr.txt je kreirana.";
-} else {
-    echo "Nisam mogao kreirati datoteku.";
+try {
+    fs.writeFileSync('kr.txt', kr + '\n');
+    console.log("Datoteka kr.txt je kreirana.");
+} catch (err) {
+    console.error("Nisam mogao kreirati datoteku.");
 }
-?>
