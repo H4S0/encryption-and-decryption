3b13cc2391d1e8598cf773c17e7dd7363a586fed
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-02-10 19:14:10 +0100

    Update dekripcija.php

--- dekripcija.php
+++ dekripcija.php
@@ -1,46 +1,43 @@
-:<?php
-$kljuc = " kljuc je: ";
-$duz = 0;
-$i = $k1 = $k2 = $k = 0;
-$kljuc1 = '';
-$linija = $pom = $kr = $pom1 = '';
+const fs = require('fs');
 
-$dat = fopen("kr.txt", "r");
-if ($dat) {
-    while (($linija = fgets($dat)) !== false) {
-        echo $linija . PHP_EOL;
-        $pom .= $linija . PHP_EOL;
+let kljuc = " kljuc je: ";
+let duz = 0;
+let i = 0, k1 = 0, k2 = 0, k = 0;
+let kljuc1 = '';
+let linija = '', pom = '', kr = '', pom1 = '';
+
+fs.readFile('kr.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Datoteka ne postoji.');
+        return;
     }
-    fclose($dat);
-} else {
-    echo "Datoteka ne postoji.";
-}
+    console.log(data);
+    pom += data + '\n';
 
-$dat2 = fopen("dekodirano.txt", "a");
-if ($dat2) {
-    for ($k = 1; $k <= 26; $k++) {
-        $pom1 = $pom;
-        for ($i = 0; $i < strlen($pom); $i++) {
-            if (ord($pom[$i]) > 96 && ord($pom[$i]) < 123) {
-                $k1 = ord($pom[$i]);
-                $k1 = $k1 - $k;
-                echo "ovo je k " . $k . PHP_EOL;
-                if ($k1 < 96) {
-                    $pom[$i] = chr($k1 + 122 - 97);
-                } else {
-                    $pom[$i] = chr($k1);
-                }
-                $kr .= $pom[$i];
-            }
-            $pom = $pom1;
+    fs.appendFile('dekodirano.txt', '', (err) => {
+        if (err) {
+            console.error('Datoteka nije kreirana.');
+            return;
         }
-        fwrite($dat2, $kr);
-        fwrite($dat2, " kljuc je " . $k . PHP_EOL);
-        $kr = "";
-    }
-    fclose($dat2);
-    echo "Datoteka dekodirano.txt je kreirana.";
-} else {
-    echo "Datoteka nije kreirana";
-}
-?>
+        for (k = 1; k <= 26; k++) {
+            pom1 = pom;
+            for (i = 0; i < pom.length; i++) {
+                if (pom.charCodeAt(i) > 96 && pom.charCodeAt(i) < 123) {
+                    k1 = pom.charCodeAt(i);
+                    k1 -= k;
+                    console.log("ovo je k " + k);
+                    if (k1 < 96) {
+                        pom = pom.substr(0, i) + String.fromCharCode(k1 + 122 - 97) + pom.substr(i + 1);
+                    } else {
+                        pom = pom.substr(0, i) + String.fromCharCode(k1) + pom.substr(i + 1);
+                    }
+                    kr += pom[i];
+                }
+                pom = pom1;
+            }
+            fs.appendFileSync('dekodirano.txt', kr + kljuc + k + '\n');
+            kr = "";
+        }
+        console.log('Datoteka dekodirano.txt je kreirana.');
+    });
+});
